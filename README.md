d00a8e3a29278ee9d4f8b658b23a559d229a7e57
Beowulfenator <beowulfenator@gmail.com>
2015-10-19 16:33:29 +0300

    Widget improvements

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # yii2-json-editor
 Yii2 wrapper for jdorn/json-editor
 
-## Usage
-    <?= JsonEditorWidget::widget([
-            'model' => $yourModel,
-            'attribute' => $yourAttribute,
-            'formId' => $yourFormId,
-            'schema' => $yourSchema
-        ])
-    ?>
+## Example
+
+    $form->field($model, 'example_field')->widget(JsonEditorWidget::className(), [
+        'schema' => $example_schema,
+        'clientOptions' => [
+            'theme' => 'bootstrap3',
+            'disable_collapse' => true,
+            'disable_edit_json' => true,
+            'disable_properties' => true,
+            'no_additional_properties' => true,
+        ],
+    ]);
--- src/JsonEditorWidget.php
+++ src/JsonEditorWidget.php
@@ -3,24 +3,89 @@
 namespace beowulfenator\JsonEditor;
 
 use yii\base\InvalidConfigException;
-use yii\widgets\InputWidget;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\helpers\Json;
+use yii\widgets\InputWidget as BaseWidget;
 
-class JsonEditorWidget extends InputWidget{
+/**
+ * Yii2 wrapper widget for jdorn/json-editor.
+ * @author Konstantin Sirotkin <beowulfenator@gmail.com>
+ * @link https://github.com/beowulfenator/yii2-json-editor
+ * @link https://github.com/jdorn/json-editor
+ * @license https://github.com/beowulfenator/yii2-json-editor/blob/master/LICENSE
+ */
+class JsonEditorWidget extends BaseWidget{
+
+    /**
+     * @var array the HTML attributes for the input tag.
+     * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
+     */
+    public $options = [];
+
+    /**
+     * An array that contains the schema to build the form from.
+     * Required. Json::encode will be used.
+     * @var array
+     */
     public $schema = null;
-    public $formId = null;
+
+    /**
+     * Id of input that will contain the resulting JSON object.
+     * Defaults to null, in which case a hidden input will be rendered.
+     * @var string|null
+     */
+    public $inputId = null;
+
+    /**
+     * @var array the HTML attributes for the widget container tag.
+     * The "tag" element specifies the tag name of the container element and defaults to "div".
+     * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
+     */
+    public $containerOptions = [];
+
+    /**
+     * Options to be passed to the client. (Schema and starting value are ignored.)
+     * List of valid options can be found here:
+     * https://github.com/jdorn/json-editor/blob/master/README.md
+     * @var array
+     */
+    public $clientOptions = [];
+
+    /**
+     * If true, a hidden input will be rendered to contain the results
+     * @var boolean
+     */
+    private $_renderInput = true;
 
     public function init(){
-        if (!$this->hasModel()) {
-            throw new InvalidConfigException("You must specify 'model' and 'attribute' properties.");
+        if ($this->name === null && !$this->hasModel() && $this->selector === null) {
+            throw new InvalidConfigException("Either 'name', or 'model' and 'attribute' properties must be specified.");
         }
+
         if (null === $this->schema) {
             throw new InvalidConfigException("You must specify 'schema' property.");
         }
-        if (null === $this->formId) {
-            throw new InvalidConfigException("You must specify 'formId' property.");
+
+        if ($this->hasModel() && !isset($this->options['id'])) {
+            $this->options['id'] = Html::getInputId($this->model, $this->attribute);
         }
+
+        if ($this->hasModel()) {
+            $this->name = empty($this->options['name']) ? Html::getInputName($this->model, $this->attribute) : $this->options['name'];
+            $this->value = Html::getAttributeValue($this->model, $this->attribute);
+        }
+
+        if (!isset($this->containerOptions['id'])) {
+            $this->containerOptions['id'] = ($this->hasModel() ? Html::getInputId($this->model, $this->attribute) : $this->getId()).'-container';
+        }
+
+        if ($this->inputId === null) {
+            $this->inputId = $this->options['id'];
+        } else {
+            $this->_renderInput = false;
+        }
+
         parent::init();
         JsonEditorAsset::register($this->getView());
     }
@@ -29,38 +94,43 @@ class JsonEditorWidget extends InputWidget{
     {
         //prepare data
         $view = $this->getView();
-        $formId = $this->formId;
-        $widgetId = $this->id;
-        $elementId = $this->options['id'];
-        $schema = Json::encode($this->schema);
 
-        if (empty($this->model->{$this->attribute})) {
-            $startVal = new \yii\web\JsExpression('null');
-        } else {
-            $startVal = $this->model->{$this->attribute};
+        //render input for results
+        if ($this->_renderInput) {
+            if ($this->hasModel()) {
+                echo Html::activeHiddenInput($this->model, $this->attribute, $this->options);
+            } else {
+                echo Html::hiddenInput($this->name, $this->value, $this->options);
+            }
         }
 
-        //placeholder
-        echo Html::activeHiddenInput($this->model, $this->attribute);
-        echo Html::tag('div', '', ['id' => $elementId.'-editor']);
+        //render editor container
+        $containerOptions = $this->containerOptions;
+        $tag = ArrayHelper::remove($containerOptions, 'tag', 'div');
+        echo Html::tag($tag, '', $containerOptions);
+
+        //prepare client options
+        $clientOptions = $this->clientOptions;
+        $clientOptions['schema'] = $this->schema;
+        ArrayHelper::remove($clientOptions, 'startval');
+        $clientOptions = Json::encode($clientOptions);
+
+        //prepare element IDs
+        $widgetId = $this->id;
+        $inputId = $this->inputId;
+        $containerId = $this->containerOptions['id'];
 
         //register js code
         $view->registerJs(
 <<<JS
-var {$widgetId} = new JSONEditor(document.getElementById('{$elementId}-editor'),{
-    "schema": {$schema},
-    "theme": "bootstrap3",
-    "disable_collapse": true,
-    "disable_edit_json": true,
-    "disable_properties": true,
-    "no_additional_properties": true,
-    "startval": {$startVal}
-});
-document.getElementById('{$formId}').addEventListener('submit',function() {
-    document.getElementById('{$elementId}').value = JSON.stringify({$widgetId}.getValue());
+var {$widgetId} = new JSONEditor(document.getElementById('{$containerId}'), {$clientOptions});
+{$widgetId}.setValue(JSON.parse(document.getElementById('{$inputId}').value));
+{$widgetId}.on('change', function() {
+    document.getElementById('{$inputId}').value = JSON.stringify({$widgetId}.getValue());
 });
 JS
-, $view::POS_END);
+, $view::POS_READY);
+
         parent::run();
     }
 }
